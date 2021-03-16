a2426ede94ddc22bccd6c0a86a8cf5d207d5476e
Bayrammyrat <bayrammurat98@gmail.com>
2021-03-15 21:21:05 -0500

    profile

--- server.js
+++ server.js
@@ -1,4 +1,28 @@
-const express = require('express')
-const app = express()
-
-app.listen(3000, () => console.log('Server Started'))
+const bodyparser = require('body-parser') 
+const express = require("express") 
+const path = require('path') 
+const app = express() 
+   
+var PORT = process.env.port || 3000 
+  
+// View Engine Setup 
+app.set("views", path.join(__dirname)) 
+app.set("view engine", "ejs") 
+  
+// Body-parser middleware 
+app.use(bodyparser.urlencoded({extended:false})) 
+app.use(bodyparser.json()) 
+   
+app.get("/profile", function(req, res){ 
+    res.render("public/profile") 
+}); 
+   
+app.post('/profile', (req, res) => { 
+    console.log("Using Body-parser: ", req.body)
+    res.render("public/success") 
+}) 
+   
+app.listen(PORT, function(error){ 
+    if (error) throw error 
+    console.log("Server created Successfully on PORT", PORT) 
+}) 
