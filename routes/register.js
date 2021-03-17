8bab5a099f0919b7e391552d4b126c2870f8da08
james1423 <63757660+james1423@users.noreply.github.com>
2021-03-17 14:51:20 -0500

    Register backend updated
    Added check for existing username

--- routes/register.js
+++ routes/register.js
@@ -1,13 +1,34 @@
 const express = require('express')
 const router = express.Router()
 
+//ARRAY FOR TESTING WITHOUT DATABASE ONLY
+const userArray = [{
+    username: 'asd123',
+    password: 'asdf1234'
+}]
+
 router.post('/', (req, res) => {
-    console.log('Trying to create new user...')
+    //console.log('Trying to create new user...')
 
-    console.log('Username: ' + req.body.username)
-    console.log('Password: ' + req.body.password)
+    //console.log('Username: ' + req.body.username)
+    //console.log('Password: ' + req.body.password)
 
-    res.redirect('login.html')
+    //Find if username already exists and adds user if username does not exist
+    const userCheck = userArray.find(index => index.username == req.body.username)
+    if (userCheck == null) {
+        const user = {
+            username: req.body.username,
+            password: req.body.password
+        }
+        userArray.push(user)
+        console.log('New user created')
+        console.log(userArray)
+
+        res.redirect('login.html')
+    } else {
+        console.log('The username ' + userCheck.username + ' is already in use')
+        res.redirect('register.html')
+    }
 })
 
 module.exports = router
--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 require('dotenv').config()
 const express = require('express')
-const app = express()
-const path = require('path') 
+const app = express() 
 app.use(express.json())
 const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -10,14 +9,15 @@ app.use(bodyParser.json())
 const PORT = process.env.PORT
 
 app.use(express.static('./public'))
-app.get('/', function(req, res){
-    res.redirect('/login.html')
+app.get('/', (req, res) => {
+    res.redirect('login.html')
 })
 
 //Register Route
 const registerRoute = require('./routes/register')
 app.use('/register.html', registerRoute)
 
+//Profile Management Route
 const profileRoute = require('./routes/profile')
 app.use('/profile.html', profileRoute)
 
