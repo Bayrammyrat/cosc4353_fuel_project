f1cb12177840dc79d033324c9b27c5c6f6aab45e
natnaelabebe1115115 <71052179+natnaelabebe1115115@users.noreply.github.com>
2021-03-18 17:22:34 -0500

    nate

--- routes/get_quote.js
+++ routes/get_quote.js
@@ -1,27 +1,33 @@
+const express = require('express')
+const router = express.Router()
 
 
-function quoteoutput (state,gallons,rateHistory){
-    var gallons_input = gallons_input.gallons;
-    var state_location = state_location.state;
-    var rate_History = rate_History.rateHistory; 
+router.post('/', (req, res) => {
+    var gallons_input = gallons_input.req.body.gallons;
+    var state_location = "TX";
+    var rate_History = true; 
     var currentPrice = 1.5;
-    
-    if (gallons_input>1000 && state_location=="TX" && rate_History==true){
-        return price-per-gallons == currentPrice+ currentPrice*0.13;
-    }else if (gallons_input>1000 && state_location=="TX"){
-        return price-per-gallons == currentPrice+ currentPrice*0.14;
-    }else if (gallons_input>1000 && rate_History==true){
-        return price-per-gallons == currentPrice+ currentPrice*0.15;
-    }else if (state_location=="TX" && rate_History==true){
-        return price-per-gallons == currentPrice+ currentPrice*0.14;
-    }else if (gallons_input>1000){
-        return price-per-gallons == currentPrice+ currentPrice*0.16;
-    }else if (state_location=="TX"){
-        return price-per-gallons == currentPrice+ currentPrice*0.15;
-    }else if (rate_History=true){
-        return price-per-gallons == currentPrice+ currentPrice*0.16;
-    }else {
-        return price-per-gallons == currentPrice+ currentPrice*0.17;
-    }
-}
 
+        if (gallons_input>1000 && state_location=="TX" && rate_History==true){
+            price_per_gallons = currentPrice+ currentPrice*0.13;
+            total_price =price_per_gallons*gallons_input;
+            
+
+        }else if (gallons_input>1000 && state_location=="TX"){
+            return price-per-gallons == currentPrice+ currentPrice*0.14;
+        }else if (gallons_input>1000 && rate_History==true){
+            return price-per-gallons == currentPrice+ currentPrice*0.15;
+        }else if (state_location=="TX" && rate_History==true){
+            return price-per-gallons == currentPrice+ currentPrice*0.14;
+        }else if (gallons_input>1000){
+            return price-per-gallons == currentPrice+ currentPrice*0.16;
+        }else if (state_location=="TX"){
+            return price-per-gallons == currentPrice+ currentPrice*0.15;
+        }else if (rate_History=true){
+            return price-per-gallons == currentPrice+ currentPrice*0.16;
+        }else {
+            return price-per-gallons == currentPrice+ currentPrice*0.17;
+        }
+    
+})
+module.exports = router
