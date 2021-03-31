29edb6d1bf4a67532d7736a577161ba238954c36
Bayrammyrat <bayrammurat98@gmail.com>
2021-03-31 18:37:19 -0500

    sql

--- server.js
+++ server.js
@@ -5,36 +5,15 @@ app.use(express.json())
 const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
-const mysql = require('mysql')
+const qbRoutes = require("./routes/qb");
 const PORT = process.env.PORT
-
-var mysqlConnection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'password',
-    database: 'UserDB',
-    multipleStatements: true
-});
-
-mysqlConnection.connect((err)=>{
-    if(!err)
-        console.log('DB connection succeded')
-    else
-        console.log('DB connection failed \n Error: ' + JSON.stringify(err,undefined,2))
-})
+app.use(qbRoutes);
 
 app.use(express.static('./public'))
 app.get('/', (req, res) => {
     res.redirect('login.html')
 })
 
-app.get('/user', (req,res)=>{
-    mysqlConnection.query("SELECT * FROM ClientInformation", (err, rows, fields) =>{
-        console.log("qwerty")
-        res.json(rows)
-    })
-    res.end()
-})
 //Login Route
 const loginRoute = require('./routes/login')
 app.use('/login.html', loginRoute)
@@ -52,5 +31,3 @@ const getQuoteRoute = require('./routes/get_quote')
 app.use('/get_quote.html', getQuoteRoute)
 
 app.listen(PORT, () => console.log('Server Started on Port ' + PORT))
-
-module.exports = mysqlConnection;
