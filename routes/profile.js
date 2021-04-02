95149d75ef4319ea1450131a4800111ca71cc71b
Bayrammyrat <bayrammurat98@gmail.com>
2021-04-01 21:15:18 -0500

    profile sql

--- routes/profile.js
+++ routes/profile.js
@@ -19,16 +19,14 @@ router.get('/:id', (req, res) => {
 })
 
 router.post('/', (req, res) => {
-    console.log("user id in profile: " + req.path + req.query + req.params)
     var fullname = req.body.fullname
     var address1 = req.body.address1
     var address2 = req.body.address2
     var city = req.body.city
     var state = req.body.state
     var zip = Number(req.body.zip)
-    var values = [1, fullname, address1, address2, city, state, zip]
 
-    var sql = "INSERT INTO ClientInformation (`id`, `fullname`, `address1`, `address2`, `city`, `state`, `zip`) VALUES ('" + 3 + "', '" + fullname + "', '" + address1 + "', '" + address2 + "', '" + city + "', '" + state + "', '" + zip + "');";
+    var sql = "INSERT INTO clientinformation (`id`, `fullname`, `address1`, `address2`, `city`, `state`, `zip`) VALUES ('" + userID + "', '" + fullname + "', '" + address1 + "', '" + address2 + "', '" + city + "', '" + state + "', '" + zip + "');";
 
     usps.verify({
         street1: address1,
